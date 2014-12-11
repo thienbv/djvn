e47ea5d9fc03106da670425353dc24493e2d45ac
Thien <thien.bui@vnetwork.vn>
2014-12-11 23:28:39 +0700

    Initial commit

---
+++ getimg.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: X-Men
+ * Date: 09/12/2014
+ * Time: 09:03
+ * http://img.youtube.com/vi/<insert-youtube-video-id-here>/0.jpg
+http://img.youtube.com/vi/<insert-youtube-video-id-here>/1.jpg
+http://img.youtube.com/vi/<insert-youtube-video-id-here>/2.jpg
+http://img.youtube.com/vi/<insert-youtube-video-id-here>/3.jpg
+ */
